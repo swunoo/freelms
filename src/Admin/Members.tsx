e5f998e7853f5b668abb882437cc8ee39b82700d
swunoo <swunhtetoo@gmail.com>
2023-12-31 11:24:47 +0900

    added modal and card, connected with table

--- src/Admin/Members.tsx
+++ src/Admin/Members.tsx
@@ -6,6 +6,9 @@ import admin_darkgray from "../assets/images/icons/admin_darkgray.png";
 import teacher_darkgray from "../assets/images/icons/teacher_darkgray.png";
 import student_darkgray from "../assets/images/icons/student_darkgray.png";
 import staff_darkgray from "../assets/images/icons/staff_darkgray.png";
+import Card from "../Common/Card";
+import { createElement, useState } from "react";
+import Modal from "../Common/Modal";
 
 export default function Members() {
     return (
@@ -52,15 +55,48 @@ function Content() {
 
 function Table() {
 
+    const [showModal, setShowModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState('Details');
+    const [modalChild, setModalChild] = useState(<></>);
+
+
+    const css_button = "flex content-center items-center gap-1 md:gap-3 py-2 md:px-5 px-3 transition rounded-xl hover:bg-neutral-200 bg-neutral-100"
+
+    const showDetails = (id: string) => {
+        const tmpData = {
+            id: '123', alias: 'jean doe', gender: 'male', class: 'biology', age: 15 };
+            console.log("clicked");
+        setModalChild(
+            <Card
+                isHor={true}
+                link='#'
+                img={student_darkgray}
+                title=''
+                content={
+                    <ul>
+                        <li>Alias: {tmpData.alias}</li>
+                        <li>Gender: {tmpData.gender}</li>
+                        <li>Class: {tmpData.class}</li>
+                        <li>Age: {tmpData.age}</li>
+                    </ul>
+                }
+                btns={[]}
+            />
+        )
+        setModalTitle(tmpData.id + ' - ' + tmpData.alias);
+        setShowModal(true);
+    }
+
+
     // tmp data
-    type dataItem = {id: string, alias: string, gender: 'male'|'female'|'other', class: string};
+    type dataItem = { id: string, alias: string, gender: 'male' | 'female' | 'other', class: string };
     const data: dataItem[] = [
-        {id: '123', alias: 'jean doe', gender: 'male', class: 'biology'},
-        {id: '343', alias: 'amelia black', gender: 'female', class: 'japanese'},
-        {id: '453454', alias: 'yamada taro', gender: 'male', class: 'maths'},
-        {id: '34534', alias: 'john doe', gender: 'female', class: 'english'},
-        {id: '54345', alias: 'dupont doe', gender: 'male', class: 'biology'},
-        {id: '55434', alias: 'john dupont', gender: 'female', class: 'chemistry'},
+        { id: '123', alias: 'jean doe', gender: 'male', class: 'biology' },
+        { id: '343', alias: 'amelia black', gender: 'female', class: 'japanese' },
+        { id: '453454', alias: 'yamada taro', gender: 'male', class: 'maths' },
+        { id: '34534', alias: 'john doe', gender: 'female', class: 'english' },
+        { id: '54345', alias: 'dupont doe', gender: 'male', class: 'biology' },
+        { id: '55434', alias: 'john dupont', gender: 'female', class: 'chemistry' },
     ];
     const keys = Object.keys(data[0])
 
@@ -69,12 +105,12 @@ function Table() {
             <table className="min-w-full text-left text-sm whitespace-nowrap">
                 <thead className="uppercase tracking-wider sticky top-0 bg-white outline outline-2 outline-neutral-200">
                     <tr>
-                        {keys.map(title => <TableHeader title={title}/>)}
+                        {keys.map(title => <TableHeader title={title} />)}
                     </tr>
                 </thead>
 
                 <tbody>
-                    {data.map(obj => <TableRow rowData={Object.values(obj)}/>)}
+                    {data.map(obj => <TableRow clickHandler={() => showDetails(obj.id)} rowData={Object.values(obj)} />)}
                 </tbody>
 
             </table>
@@ -130,34 +166,37 @@ function Table() {
                     </li>
                 </ul>
             </nav>
+
+            <Modal child={modalChild} title={modalTitle} showModal={showModal} setShowModal={setShowModal}/>
+         
         </div>
     )
 }
 
-function TableHeader({title}: {title: string}){
+function TableHeader({ title }: { title: string }) {
     return (
         <th scope="col" className="px-6 py-4">
-        {title}
-        <a href="" className="inline">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-                className="w-[0.75rem] h-[0.75rem] inline ml-1 text-neutral-500 dark:text-neutral-200 mb-[1px]"
-                fill="currentColor"
-            >
-                {/* Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
-                <path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z" />
-            </svg>
-        </a>
+            {title}
+            <a href="" className="inline">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                    className="w-[0.75rem] h-[0.75rem] inline ml-1 text-neutral-500 dark:text-neutral-200 mb-[1px]"
+                    fill="currentColor"
+                >
+                    {/* Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
+                    <path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z" />
+                </svg>
+            </a>
 
-    </th>
+        </th>
     )
 }
 
-function TableRow({rowData}: {rowData: string[]}){
+function TableRow({ clickHandler, rowData }: { clickHandler:()=>void, rowData: string[] }) {
     return (
-        <tr className="border-b hover:bg-neutral-100">
-            {rowData.map(data => <td className="px-6 py-4">{data}</td>)}            
+        <tr className="border-b hover:bg-neutral-100" onClick={clickHandler}>
+            {rowData.map(data => <td className="px-6 py-4">{data}</td>)}
         </tr>
     )
 }
---
+++ src/Common/Card.tsx
@@ -0,0 +1,30 @@
+import { MouseEventHandler, ReactNode } from "react"
+
+type btnData = {onclick: MouseEventHandler<HTMLButtonElement>|undefined, text: string, color: string}
+type cardProps = {isHor: boolean, link: string, img: string|null, title:string, content:ReactNode, btns: btnData[]}
+
+
+export default function Card(props: cardProps) {
+    return (
+        <div className={(!props.isHor?"flex-col ":"") + "flex rounded-lg bg-white text-left overflow-hidden"}>
+            <a href={props.link}>
+                {props.img && <img className="w-full" src={props.img} alt="" />}
+            </a>
+            <div className="p-6">
+                <h5 className="mb-2 text-xl font-bold tracking-wide text-neutral-800">
+                    {props.title}
+                </h5>
+                {props.content}
+            </div>
+
+            {
+            props.btns.map(btn => {
+                return(
+                    <button onClick={btn.onclick}
+                    className={btn.color + " mt-3 inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-lg transition duration-150 ease-in-out hover:brightness-90"}> {btn.text} </button>
+                )
+            })}
+
+        </div>
+    )
+}
