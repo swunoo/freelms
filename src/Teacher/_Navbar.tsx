72f44780166f62b6b1636aa4db99114202e25daa
swunoo <swunhtetoo@gmail.com>
2024-01-03 15:49:05 +0900

    added sidebar for pc

--- src/Teacher/_Navbar.tsx
+++ src/Teacher/_Navbar.tsx
@@ -1,11 +1,12 @@
 import { ReactNode, useEffect, useState } from "react";
-import { IconBtn } from "../Common/Buttons";
-import { mockChat, mockLiveSessionList, mockNoti } from "../Mockers";
+import { FullBtn, IconBtn } from "../Common/Buttons";
+import { mockChat, mockChatDetails, mockLiveSessionList, mockNoti } from "../Mockers";
 import chatIcon from "../assets/images/icons/chat_blue.png"
 import notiIcon from "../assets/images/icons/noti_blue.png"
 import calendarIcon from "../assets/images/icons/calendar_blue.png"
 import Calendar from "../Common/Calendar";
 import { LiveSession } from "../Common/Sidebar";
+import { Input } from "../Common/Inputs";
 
 export default Navbar;
 
@@ -29,7 +30,12 @@ function Navbar({active, setSideBarContent}: {active: string, setSideBarContent:
 
     const openChats = () => {
         const chatData = mockChat;
-        setSideBarContent(<ChatBox data={chatData} />);
+        setSideBarContent(<ChatBox data={chatData} toChatDetails={openMessageDetails}/>);
+    }
+
+    const openMessageDetails = (id: string, name: string, photo: string) => {
+        const chatDetails = mockChatDetails;
+        setSideBarContent(<ChatDetails name={name} img={photo} data={chatDetails} goBack={openChats}/>);
     }
 
     const openNotis = () => {
@@ -37,7 +43,7 @@ function Navbar({active, setSideBarContent}: {active: string, setSideBarContent:
         setSideBarContent(<NotiBox data={notiData} />);
     }
 
-    useEffect(openCalendar,[])
+    // useEffect(openCalendar,[])
 
     return (
         <div
@@ -59,11 +65,11 @@ function Navbar({active, setSideBarContent}: {active: string, setSideBarContent:
 }
 
 type chatInter = any; // TODO;
-export function ChatBox({data}: {data: chatInter[]}){
+function ChatBox({data, toChatDetails}: {data: chatInter[], toChatDetails: (i:string, n:string, p:string)=>void}){
     return (
         <>
             {data.map(c => (
-                <div className="grid grid-cols-6 gap-3 border-b border-gray-300 p-3 cursor-pointer hover:bg-gray-200">
+                <div onClick={() => toChatDetails(c.id, c.name, c.img)} className="grid grid-cols-6 gap-3 border-b border-gray-300 p-3 cursor-pointer hover:bg-gray-200">
                     <img src={c.img}/>
                     <div className="col-start-2 col-end-7 ">
                         <div className="grid grid-cols-4">
@@ -79,8 +85,48 @@ export function ChatBox({data}: {data: chatInter[]}){
     )
 }
 
+type chatDetailInter = any; // TODO;
+function ChatDetails({name, img, data, goBack}: {name: string, img: string, data: chatDetailInter[], goBack: ()=>void}){
+    return (
+        <div
+            className="relative w-full overflow-scroll"
+        >
+            <div className="flex gap-3 h-16 items-center border-b-2 border-gray-300 px-2 sticky top-0 bg-white">
+                <IconBtn icon={chatIcon} onclick={goBack}/>
+                <div className="flex gap-3 items-center">
+                    <img className="h-12" src={img}/>
+                    <p className="font-bold text-base">{name}</p>
+                </div>
+            </div>
+
+
+            <div className="overflow-y-scroll bg-yellow-300 h-96">
+                {data.map(c => (
+                        <p className={
+                            (!c.isReceived ? "ml-auto bg-gray-200" : "bg-blue-100")
+                            + " w-3/4 rounded-xl p-2 m-3"
+                        }>
+                            {c.msg}
+                            <span className="font-thin text-xs block text-right">{c.datetime}</span>
+                        </p>
+                ))
+                }
+            </div>
+
+
+            <div className="border flex gap-3 p-2 bg-gray-300 justify-center m-auto sticky bottom-0">
+                <input name={name} type="text" className="w-48 rounded-lg py-2 pl-3 border text-sm focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-400" />
+                <FullBtn label="Send"/>
+            </div>
+
+        </div>
+    )
+}
+
+
+
 type notiInter = any; // TODO;
-export function NotiBox({data}: {data: notiInter[]}){
+function NotiBox({data}: {data: notiInter[]}){
 
     return (
         <>
