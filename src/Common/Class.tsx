753e6d6d64ed5500a7fac8e9fcb3ed4a0f0b9d5f
swunoo <swunhtetoo@gmail.com>
2024-01-04 10:59:32 +0900

    added meta edit

--- src/Common/Class.tsx
+++ src/Common/Class.tsx
@@ -78,7 +78,31 @@ export function SectionContentEdit({content, title, toView}: {content: string, t
 export function ClassMetaEdit({classData}: classType){
     return (
         <div className="">
+            <Input label="Course Title" name="sectionTitle" value={classData.title} />
+            <div className="flex justify-end gap-3 py-3 border-b">
+                <FullBtn label="Add Unit"/>
+            </div>
+            {classData.units.map((unit: ClassUnit) => (
+                <div className="rounded-xl py-3 px-5 bg-gray-200 shadow my-5">
+                    <Input label="Unit Title" name="sectionTitle" value={unit.title} />
+                <div className="flex justify-end gap-3 py-3 border-b">
+                    <FullBtn label="Delete Unit"/>
+                    <FullBtn label="Add Section"/>
+                </div>
+                <ul className="ml-8">
+                        {unit.sections.map(section => (
+                            <li
+                            className="list-decimal my-3 cursor-pointer flex justify-between"
+                            >
+                                <span>{section.title}</span>
+                                <FullBtn label="Delete Section"/>
+                            </li>
+                        ))
+                        }
+                </ul>
+                </div>
 
+            ))}
         </div>
     )
 }
