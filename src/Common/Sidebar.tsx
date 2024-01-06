c8187129bca151f3146f5f530ff4619b3c97143e
swunoo <swunhtetoo@gmail.com>
2024-01-06 15:15:35 +0900

    added auth screen

--- src/Common/Sidebar.tsx
+++ src/Common/Sidebar.tsx
@@ -10,7 +10,7 @@ export default function Sidebar({sideBarContent}: {sideBarContent: ReactNode}){
 
     return (
         <>
-            <div className={(showBar?"":"hidden ")+" h-screen w-screen m-0 md:h-fit md:static md:w-1/4 border-2 rounded-xl md:m-2 bg-white overflow-hidden fixed md:shrink-0"}
+            <div className={(showBar?"":"hidden ")+" h-screen w-screen m-0 md:ml-auto md:m-3 md:h-fit md:static md:w-1/5 border-2 rounded-xl bg-white overflow-hidden fixed md:shrink-0"}
             >
                 {sideBarContent}
             </div>
