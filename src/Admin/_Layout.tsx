59a135ab3b53aceb5f8fbb12ff54dec29f2975ec
swunoo <swunhtetoo@gmail.com>
2024-01-06 16:40:24 +0900

    replaced Admin chatPanel with sideBar

---
+++ src/Admin/_Layout.tsx
@@ -0,0 +1,30 @@
+import Calendar from "../Common/Calendar";
+import Card from "../Common/Card";
+import ChatPanel from "../Common/ChatPanel";
+import SearchBar from "../Common/Inputs";
+import { hrsToTimeStr } from "../Common/Utils";
+import { mockClassList, mockLiveSessionList } from "../Mockers";
+import Navbar from "./_Navbar";
+import Sidebar, { LiveSession } from "../Common/Sidebar";
+import { MouseEvent, ReactNode, useEffect, useState } from "react";
+
+export default function Layout({ children, active }: { children: ReactNode, active: string }) {
+
+    const [sideBarContent, setSideBarContent] = useState<ReactNode | undefined>(undefined);
+
+    return (
+        <div>
+            <div className="w-full relative">
+                <Navbar active={active} setSideBarContent={setSideBarContent} />
+                <div className="min-h-screen flex bg-white">
+                    <div id="desk-content" className="w-full">
+                        {children}
+                    </div>
+
+                    <Sidebar sideBarContent={sideBarContent}/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- src/Admin/_Navbar.tsx
+++ src/Admin/_Navbar.tsx
@@ -1,22 +1,49 @@
-
+import { ReactNode, useEffect } from "react";
+import { ChatBox, ChatDetails, NotiBox } from "../Common/ChatPanel";
+import { mockChat, mockChatDetails, mockNoti } from "../Mockers";
+import calendarIcon from "../assets/images/icons/calendar_blue.png";
+import chatIcon from "../assets/images/icons/chat_blue.png";
+import notiIcon from "../assets/images/icons/noti_blue.png";
+import { IconBtn } from "../Common/Buttons";
 
 export default Navbar;
 
-type links = 'dashboard' | 'design' | 'members';
 
 /* Navbar for Admin */
-function Navbar({active}: {active: links}){
-
+function Navbar({ active, setSideBarContent }: { active: string, setSideBarContent: (r: ReactNode)=>void}) {
+    const openChats = () => {
+        const chatData = mockChat;
+        setSideBarContent(<ChatBox data={chatData} toChatDetails={openMessageDetails} />);
+    }
+    
+    const openMessageDetails = (id: string, name: string, photo: string) => {
+        const chatDetails = mockChatDetails;
+        setSideBarContent(<ChatDetails name={name} img={photo} data={chatDetails} goBack={openChats} />);
+    }
+    
+    const openNotis = () => {
+        const notiData = mockNoti;
+        setSideBarContent(<NotiBox data={notiData} />);
+    }
+    
+    useEffect(openChats, [])
+    
     return (
         <div
-            className="flex justify-between py-3 px-5 md:px-16 text-base"
+            className="flex justify-between items-center py-2 px-5 md:px-16 text-base sticky top-0 w-full shadow-sm bg-slate-100 z-20"
         >
             <p>freeLMS</p>
-            <div className="flex gap-7">
-                <a href="/admin/dashboard" className={active==='dashboard'?'font-bold':''}>Dashboard</a>
-                <a href="/admin/design" className={active==='design'?'font-bold':''}>Design</a>
-                <a href="/admin/members" className={active==='members'?'font-bold':''}>Members</a>
+            <div className="flex md:gap-7 gap-3 items-center relative">
+                <a href="/admin/dashboard" className={active === 'dashboard' ? 'font-bold' : ''}>Dashboard</a>
+                <a href="/admin/design" className={active === 'design' ? 'font-bold' : ''}>Design</a>
+                <a href="/admin/members" className={active === 'members' ? 'font-bold' : ''}>Members</a>
+    
+                <div className="md:ml-7 flex md:gap-7 h-fit">
+                    <IconBtn icon={chatIcon} onclick={openChats} />
+                    <IconBtn icon={notiIcon} onclick={openNotis} />
+                </div>
             </div>
         </div>
     )
 }
+
